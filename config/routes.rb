a094d81f57886b16ec23c103297e68782b7baa5a
integgy <matthewregidor@gmail.com>
2024-02-01 17:01:29 -0800

    create frontend

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  namespace :api, defaults: {format: json} do
+  namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:show, :create, :destroy]
 
